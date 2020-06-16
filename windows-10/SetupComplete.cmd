3b9ed34bee23445e09c73023e8db34c292f89231
SquelchPlop <matthewhadley16@hotmail.com>
2020-06-16 20:09:40 +0100

    feat: add windows update task to image

--- windows-10/SetupComplete.cmd
+++ windows-10/SetupComplete.cmd
@@ -1,9 +1,15 @@
 cd %TEMP%
 
+REM Install Windows updates
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/windows_update/apply_windows_updates_no_reboot.ps1'))"
+
+REM Remove bloatware
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/apps_remove.ps1'))"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/features_remove.ps1'))"
 
+REM Install JumpCloud agent
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "./InstallWindowsAgent.ps1 -JumpCloudConnectKey 'xxx'"
 
+REM Apply Windows Security Baseline
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/windows_security_baselines/apply_wsb_win10_nondomain.ps1'))"
